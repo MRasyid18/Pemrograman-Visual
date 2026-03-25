d968cff89a9fd87ceca060b06d0d32c23a14958e
MRasyid18 <mrasyid18102005@gmail.com>
2026-03-26 00:13:21 +0800

    Tugas 3

---
+++ Tugas Mandiri 3/Tugas Mandiri 3/Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form_input
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Panel1 = New Panel()
+        Label2 = New Label()
+        Label1 = New Label()
+        Panel2 = New Panel()
+        Label3 = New Label()
+        Label4 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        Timer1 = New Timer(components)
+        Panel1.SuspendLayout()
+        Panel2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(Panel2)
+        Panel1.Controls.Add(Label2)
+        Panel1.Controls.Add(Label1)
+        Panel1.Location = New Point(235, 2)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(339, 436)
+        Panel1.TabIndex = 0
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.ForeColor = SystemColors.ButtonHighlight
+        Label2.Location = New Point(14, 103)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(123, 28)
+        Label2.TabIndex = 1
+        Label2.Text = "Form input"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.White
+        Label1.Location = New Point(115, 23)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(221, 28)
+        Label1.TabIndex = 0
+        Label1.Text = "VISUAL STUDIO 2026"
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = SystemColors.ControlText
+        Panel2.Controls.Add(Button1)
+        Panel2.Controls.Add(TextBox2)
+        Panel2.Controls.Add(TextBox1)
+        Panel2.Controls.Add(Label4)
+        Panel2.Controls.Add(Label3)
+        Panel2.Location = New Point(1, 160)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(339, 281)
+        Panel2.TabIndex = 2
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.Transparent
+        Label3.ForeColor = SystemColors.ControlLightLight
+        Label3.Location = New Point(37, 59)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(75, 20)
+        Label3.TabIndex = 0
+        Label3.Text = "Username"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.ForeColor = SystemColors.ControlLightLight
+        Label4.Location = New Point(37, 106)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(70, 20)
+        Label4.TabIndex = 1
+        Label4.Text = "Password"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(142, 59)
+        TextBox1.MaxLength = 6
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(142, 106)
+        TextBox2.MaxLength = 10
+        TextBox2.Name = "TextBox2"
+        TextBox2.PasswordChar = "."c
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.ActiveCaptionText
+        Button1.FlatAppearance.BorderColor = Color.White
+        Button1.FlatAppearance.BorderSize = 3
+        Button1.FlatAppearance.MouseDownBackColor = Color.DarkOrchid
+        Button1.FlatAppearance.MouseOverBackColor = Color.Violet
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.ForeColor = SystemColors.ControlLightLight
+        Button1.Location = New Point(173, 176)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 36)
+        Button1.TabIndex = 4
+        Button1.Text = "Log In"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        Timer1.Interval = 10
+        ' 
+        ' Form_input
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.GrayText
+        ClientSize = New Size(800, 450)
+        Controls.Add(Panel1)
+        ForeColor = SystemColors.ControlText
+        Name = "Form_input"
+        Text = "Form1"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Timer1 As Timer
+
+End Class
---
+++ Tugas Mandiri 3/Tugas Mandiri 3/Form1.vb
@@ -0,0 +1,65 @@
+﻿Public Class Form_input
+    Dim x, y As Integer
+    Dim newpoint As New System.Drawing.Point
+
+    ' Saat mouse ditekan di form
+    Private Sub Form_input_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+        x = Control.MousePosition.X - Me.Location.X
+        y = Control.MousePosition.Y - Me.Location.Y
+    End Sub
+
+    ' Saat mouse digerakkan di form
+    Private Sub Form_input_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        If e.Button = MouseButtons.Left Then
+            newpoint = Control.MousePosition
+            newpoint.X -= (x)
+            newpoint.Y -= (y)
+            Me.Location = newpoint
+        End If
+    End Sub
+
+    ' Panel1 dapat di-drag
+    Private Sub Panel1_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel1.MouseDown
+        x = Control.MousePosition.X - Me.Location.X
+        y = Control.MousePosition.Y - Me.Location.Y
+    End Sub
+
+    Private Sub Panel1_MouseMove(sender As Object, e As MouseEventArgs) Handles Panel1.MouseMove
+        If e.Button = MouseButtons.Left Then
+            newpoint = Control.MousePosition
+            newpoint.X -= (x)
+            newpoint.Y -= (y)
+            Me.Location = newpoint
+        End If
+    End Sub
+
+    ' Panel2 dapat di-drag
+    Private Sub Panel2_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel2.MouseDown
+        x = Control.MousePosition.X - Me.Location.X
+        y = Control.MousePosition.Y - Me.Location.Y
+    End Sub
+
+    Private Sub Panel2_MouseMove(sender As Object, e As MouseEventArgs) Handles Panel2.MouseMove
+        If e.Button = MouseButtons.Left Then
+            newpoint = Control.MousePosition
+            newpoint.X -= (x)
+            newpoint.Y -= (y)
+            Me.Location = newpoint
+        End If
+    End Sub
+
+    ' Animasi Panel2 saat form dibuka
+    Private Sub Form_input_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Panel2.Left = Me.Width
+    End Sub
+
+    ' Timer menggerakkan Panel2
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If Panel2.Left > 12 Then
+            Panel2.Left = Int(Panel2.Left - 20)
+        Else
+            Panel2.Left = 12
+        End If
+    End Sub
+
+End Class
